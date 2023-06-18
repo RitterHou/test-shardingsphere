88c53bd5ad25374d57b22c9d3a18a75ef1987ce5
derobukal <derobukal@gmail.com>
2023-06-18 20:08:49 +0800

    添加数据库相关配置

---
+++ src/main/java/com/nosuchfield/mapper/OrderMapper.java
@@ -0,0 +1,24 @@
+package com.nosuchfield.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface OrderMapper {
+
+    /**
+     * 插入
+     * @param order
+     */
+    void insert(Order order);
+
+    /**
+     * 批量查询
+     * @param idList
+     * @return
+     */
+    List<Order> selectListByIds(@Param("idList") List<Long> idList);
+
+}
