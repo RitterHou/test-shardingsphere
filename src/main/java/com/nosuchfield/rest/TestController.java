2f13beacbce287ef458d51f1bc8cbd6f81a02c05
derobukal <derobukal@gmail.com>
2023-06-18 18:50:01 +0800

    优化代码

--- src/main/java/com/nosuchfield/rest/TestController.java
+++ src/main/java/com/nosuchfield/rest/TestController.java
@@ -2,6 +2,7 @@ package com.nosuchfield.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/test1")
-    public String test() {
-        return "hello, world";
+    public String test(@RequestParam(value = "name", defaultValue = "world") String name) {
+        return String.format("hello, %s!", name);
     }
 
 }
