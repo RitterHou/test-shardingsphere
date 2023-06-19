2c08572642eee9f58a6f418e22bbc4de3e681644
derobukal <derobukal@gmail.com>
2023-06-19 22:26:27 +0800

    修改代码

--- src/main/java/com/nosuchfield/Application.java
+++ src/main/java/com/nosuchfield/Application.java
@@ -1,9 +1,11 @@
 package com.nosuchfield;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.nosuchfield.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
